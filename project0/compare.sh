0f290271e3000c8ee87dae5b13d643781150f2ca
Minh Pham <minh.pham@utexas.edu>
2012-09-17 16:40:49 -0500

    Added -a flag to diff command to treat files as texts (instead of binaries).

--- project0/compare.sh
+++ project0/compare.sh
@@ -4,6 +4,6 @@ for file in $(find ./ -name "trace*.txt"); do
     echo "Tracing "$file
     ./sdriver.pl -t $file -s ./msh > msh-trace.out -a "-p"
     ./sdriver.pl -t $file -s ./mshref > mshref-trace.out -a "-p"
-    diff -y msh-trace.out mshref-trace.out
+    diff -y -a msh-trace.out mshref-trace.out
     echo "====================================================================="
 done | tee trace-diff.out
