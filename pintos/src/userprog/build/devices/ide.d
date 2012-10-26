2485102ad442b527bf5889130ab57a8dc1d2dfcf
Minh Pham <minh.pham@utexas.edu>
2012-10-26 11:00:28 -0500

    Removed build files from repo.

--- pintos/src/userprog/build/devices/ide.d
+++
@@ -1,8 +0,0 @@
-devices/ide.o: ../../devices/ide.c ../../devices/ide.h ../../lib/ctype.h \
- ../../lib/debug.h ../../lib/stdbool.h ../../lib/stdio.h \
- ../../lib/stdarg.h ../../lib/stddef.h ../../lib/stdint.h \
- ../../lib/kernel/stdio.h ../../devices/block.h ../../lib/inttypes.h \
- ../../devices/partition.h ../../devices/timer.h ../../lib/round.h \
- ../../threads/thread.h ../../lib/kernel/list.h ../../threads/synch.h \
- ../../filesys/file.h ../../filesys/off_t.h ../../threads/io.h \
- ../../threads/interrupt.h
--- pintos/src/userprog/build/devices/speaker.d
+++
@@ -1,6 +0,0 @@
-devices/speaker.o: ../../devices/speaker.c ../../devices/speaker.h \
- ../../devices/pit.h ../../lib/stdint.h ../../threads/io.h \
- ../../lib/stddef.h ../../threads/interrupt.h ../../lib/stdbool.h \
- ../../devices/timer.h ../../lib/round.h ../../threads/thread.h \
- ../../lib/debug.h ../../lib/kernel/list.h ../../threads/synch.h \
- ../../filesys/file.h ../../filesys/off_t.h
