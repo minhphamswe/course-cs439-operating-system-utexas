cb766c89fbd7b35410a1cae75583467c7c080808
Minh Pham <minh.pham@utexas.edu>
2012-10-26 12:51:11 -0500

    Added documentation from syscall into syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -25,29 +25,17 @@ void sysclose_handler (struct intr_frame *f);
 struct fileHandle* get_handle (int fd);
 void terminate_thread();
 
-/* Read a byte at the user virtual address UADDR
-* UADDR must be below PHYS_BASE
-* Returns the byte value if successful, -1 if a segfault occurred.
-*/
-static int
-get_user_byte (const uint8_t *uaddr) {
-  int result;
-  asm ( "movl $1f, %0; movzbl %1, %0; 1:"
-        : "=&a" (result)
-        : "m" (*uaddr));
-  return result;
-}
-
 /* Read 4 bytes at the user virtual address UADDR
 * UADDR must be below PHYS_BASE
 * Returns the byte value if successful, -1 if a segfault occurred.
 */
 static int
 get_user (const uint32_t *uaddr) {
-  int result;
-  asm ( "movl $1f, %0; movl %1, %0; 1:"
-        : "=&a" (result)
-        : "m" (*uaddr));
+  int result = -1;
+  if (uaddr < PHYS_BASE)
+    asm ( "movl $1f, %0; movl %1, %0; 1:"
+          : "=&a" (result)
+          : "m" (*uaddr));
   return result;
 }
 
@@ -188,14 +176,28 @@ syscall_handler (struct intr_frame *f)
 }
 
 
-// Thread turns off system
+/**
+ * Thread turns off system.
+ * 
+ * Terminates Pintos by calling shutdown_power_off() (declared in
+ * devices/shutdown.h). This should be seldom used, because you lose some
+ * information about possible deadlock situations, etc.
+ */
 void syshalt_handler(struct intr_frame *f)
 {
   // Turn off pintos
   shutdown_power_off();
 }
 
-// Thread calls exit()
+
+/**
+ * Thread calls exit()
+ *
+ * Terminates the current user program, returning status to the kernel. If
+ * the process's parent waits for it (see below), this is the status that
+ * will be returned. Conventionally, a status of 0 indicates success and
+ * nonzero values indicate errors.
+ */
 void sysexit_handler(struct intr_frame *f)
 {
   // Get the exit return value and set it.
@@ -206,7 +208,16 @@ void sysexit_handler(struct intr_frame *f)
   thread_exit();
 }
 
-// Thread calls sysexec
+/**
+ * Thread calls sysexec
+ *
+ * Runs the executable whose name is given in cmd_line, passing any given
+ * arguments, and returns the new process's program id (pid). Must return
+ * pid -1, which otherwise should not be a valid pid, if the program cannot
+ * load or run for any reason. Thus, the parent process cannot return from
+ * the exec until it knows whether the child process successfully loaded its
+ * executable. You must use appropriate synchronization to ensure this.
+ */
 void sysexec_handler(struct intr_frame *f)
 {
   char *cmdline = pop_stack(f);
@@ -218,7 +229,45 @@ void sysexec_handler(struct intr_frame *f)
   f->eax = newtid;
 }
 
-// Thread calls syswait
+/**
+ * Thread calls syswait
+ *
+ * Waits for a child process pid and retrieves the child's exit status.
+ * If pid is still alive, waits until it terminates. Then, returns the status
+ * that pid passed to exit. If pid did not call exit(), but was terminated by
+ * the kernel (e.g. killed due to an exception), wait(pid) must return -1.
+ * It is perfectly legal for a parent process to wait for child processes
+ * that have already terminated by the time the parent calls wait, but the
+ * kernel must still allow the parent to retrieve its child's exit status, or
+ * learn that the child was terminated by the kernel.
+ *
+ * wait must fail and return -1 immediately if any of the following conditions
+ * are true:
+ *   - pid does not refer to a direct child of the calling process. pid is a
+ *     direct child of the calling process if and only if the calling process
+ *     received pid as a return value from a successful call to exec.
+ *     Note that children are not inherited: if A spawns child B and B spawns
+ *     child process C, then A cannot wait for C, even if B is dead. A call
+ *     to wait(C) by process A must fail. Similarly, orphaned processes are
+ *     not assigned to a new parent if their parent process exits before they
+ *     do.
+ *   - The process that calls wait has already called wait on pid. That is, a
+ *     process may wait for any given child at most once.
+ * Processes may spawn any number of children, wait for them in any order,
+ * and may even exit without having waited for some or all of their children.
+ *
+ * Your design should consider all the ways in which waits can occur. All of
+ * a process's resources, including its struct thread, must be freed whether
+ * its parent ever waits for it or not, and regardless of whether the child
+ * exits before or after its parent.
+ *
+ * You must ensure that Pintos does not terminate until the initial process
+ * exits. The supplied Pintos code tries to do this by calling process_wait()
+ * (in userprog/process.c) from main() (in threads/init.c). We suggest that
+ * you implement process_wait() according to the comment at the top of the
+ * function and then implement the wait system call in terms of
+ * process_wait().
+ */
 void syswait_handler(struct intr_frame *f)
 {
   // Get PID from stack
@@ -227,7 +276,13 @@ void syswait_handler(struct intr_frame *f)
   //printf("TID status: %d\n", status);
 }
 
-// Thread calls syscreate
+/** Thread calls syscreate
+ *
+ * Creates a new file called file initially initial_size bytes in size.
+ * Returns true if successful, false otherwise. Creating a new file does not
+ * open it: opening the new file is a separate operation which would require
+ * a open system call.
+ */
 void syscreate_handler(struct intr_frame *f)
 {
   // Get file name and size from stack
@@ -244,7 +299,14 @@ void syscreate_handler(struct intr_frame *f)
   f->eax = status;
 }
 
-// Thread calls sysremove
+/**
+ * Thread calls sysremove
+ *
+ * Deletes the file called file. Returns true if successful, false otherwise.
+ * A file may be removed regardless of whether it is open or closed, and
+ * removing an open file does not close it. See Removing an Open File, for
+ * details.
+ */
 void sysremove_handler(struct intr_frame *f)
 {
   // Get file name from stack
@@ -257,7 +319,24 @@ void sysremove_handler(struct intr_frame *f)
   f->eax = status;
 }
 
-// Thread calls sysopen
+/** Thread calls sysopen
+ *
+ * Opens the file called file. Returns a nonnegative integer handle called a
+ * "file descriptor" (fd), or -1 if the file could not be opened. File
+ * descriptors numbered 0 and 1 are reserved for the console: fd 0
+ * (STDIN_FILENO) is standard input, fd 1 (STDOUT_FILENO) is standard output.
+ * The open system call will never return either of these file descriptors,
+ * which are valid as system call arguments only as explicitly described
+ * below.
+ *
+ * Each process has an independent set of file descriptors. File descriptors
+ * are not inherited by child processes.
+ *
+ * When a single file is opened more than once, whether by a single process
+ * or different processes, each open returns a new file descriptor. Different
+ * file descriptors for a single file are closed independently in separate
+ * calls to close and they do not share a file position.
+ */
 void sysopen_handler(struct intr_frame *f)
 {
   // Get file name
@@ -291,7 +370,11 @@ void sysopen_handler(struct intr_frame *f)
   }
 }
 
-// Thread call sysfilesize
+/**
+ * Thread call sysfilesize
+ *
+ * Returns the size, in bytes, of the file open as fd.
+ */
 void sysfilesize_handler(struct intr_frame *f)
 {
   // Get fd from stack
@@ -312,19 +395,26 @@ void sysfilesize_handler(struct intr_frame *f)
   f->eax -1;
 }
 
-// Thread calls sysread
+/**
+ * Thread calls sysread
+ *
+ * Reads size bytes from the file open as fd into buffer. Returns the number
+ * of bytes actually read (0 at end of file), or -1 if the file could not be
+ * read (due to a condition other than end of file). Fd 0 reads from the
+ * keyboard using input_getc().
+ */
 void sysread_handler(struct intr_frame *f)
 {
   // Get arguments from the stack
-  int fd = (int) pop_stack(f);
-  void *buffer = (void*) pop_stack(f);
+  int fd = pop_stack(f);
+  void *buffer = pop_stack(f);
   off_t size = pop_stack(f);
 
   // Number of characters read
   int ret = 0;
 
   // Check to make sure buffer is in user space
-  if (buffer > PHYS_BASE || get_user(buffer) == -1) {
+  if (get_user(buffer) == -1) {
     terminate_thread();
   }
 
@@ -361,7 +451,25 @@ void sysread_handler(struct intr_frame *f)
   }
 }
 
-// Thread calls syswrite
+/**
+ * Thread calls syswrite
+ *
+ * Writes size bytes from buffer to the open file fd. Returns the number of
+ * bytes actually written, which may be less than size if some bytes could
+ * not be written.
+ * 
+ * Writing past end-of-file would normally extend the file, but file growth
+ * is not implemented by the basic file system. The expected behavior is to
+ * write as many bytes as possible up to end-of-file and return the actual
+ * number written, or 0 if no bytes could be written at all.
+ *
+ * Fd 1 writes to the console. Your code to write to the console should write
+ * all of buffer in one call to putbuf(), at least as long as size is not
+ * bigger than a few hundred bytes. (It is reasonable to break up larger
+ * buffers.) Otherwise, lines of text output by different processes may end
+ * up interleaved on the console, confusing both human readers and our
+ * grading scripts.
+ */
 void syswrite_handler(struct intr_frame *f)
 {
   // Get the number of the file descriptor to write buffer to
@@ -396,7 +504,20 @@ void syswrite_handler(struct intr_frame *f)
   //printf("Exiting Syswrite Handler\n");
 }
 
-// Thread calls sysseek
+/**
+ * Thread calls sysseek
+ *
+ * Changes the next byte to be read or written in open file fd to position,
+ * expressed in bytes from the beginning of the file. (Thus, a position of 0
+ * is the file's start.)
+ * 
+ * A seek past the current end of a file is not an error. A later read
+ * obtains 0 bytes, indicating end of file. A later write extends the file,
+ * filling any unwritten gap with zeros. (However, in Pintos, files will have
+ * a fixed length until project 4 is complete, so writes past end of file
+ * will return an error.) These semantics are implemented in the file system
+ * and do not require any special effort in system call implementation.
+ */
 void sysseek_handler(struct intr_frame *f)
 {
   // Get arguments from the stack
@@ -418,7 +539,12 @@ void sysseek_handler(struct intr_frame *f)
   }
 }
 
-// Thread calls systell
+/**
+ * Thread calls systell
+ *
+ * Returns the position of the next byte to be read or written in open file
+ * fd, expressed in bytes from the beginning of the file.
+ */
 void systell_handler(struct intr_frame *f)
 {
   // Get the file descriptor from the stack
@@ -440,7 +566,13 @@ void systell_handler(struct intr_frame *f)
   f->eax = -1;
 }
 
-// Thread calls sysclose
+/**
+ * Thread calls sysclose
+ *
+ * Closes file descriptor fd. Exiting or terminating a process implicitly
+ * closes all its open file descriptors, as if by calling this function for
+ * each one.
+ */
 void sysclose_handler(struct intr_frame *f)
 {
   // Get the file descriptor from the stack
