9227ecd681bd7ab4ee0a23a92bc56665e45f77fd
Minh Pham <minh.pham@utexas.edu>
2012-12-09 05:00:49 -0600

    Updated the test script to make clean before running make check.

--- pintos/src/filesys/run-test.sh
+++ pintos/src/filesys/run-test.sh
@@ -4,6 +4,7 @@ TIME_STAMP=$(date "+%y-%m-%d_%H-%M-%S")
 
 for (( i=1; i<=$NUM_TIMES; i++ ))
 do
-  file=build_"$TIME_STAMP"_"$i".log
+  build_"$TIME_STAMP"_"$i".log
+  make clean
   make check 2>&1 > $file
 done
